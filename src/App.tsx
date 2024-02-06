d6871fe7a8f7ef353d9c9b68675ec4a51972a58e
AndreyUrazaev <f.a.z.a@bk.ru>
2024-02-06 15:49:17 +0500

    lesson up to 10 (out of 100) moving project

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import {Tachnologies} from "./components/Tachnologies";
 import {Header} from "./components/Header";
 import {Footer} from "./components/Footer";
 
+console.log('alalalla')
+
 function App() {
-  return (
-    <div>
-      <Header/>
-      <Tachnologies/>
-      <Footer/>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Tachnologies/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
